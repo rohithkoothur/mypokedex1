fb2d703256fe02b6fddb43be9e01eedad42bdc4e
rohithkoothur <rohithedat7@gmail.com>
2021-08-27 14:54:46 +0530

    searched again

---
+++ styles/mystyles.js
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled'
+export const card = styled.div`
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+`
+
+export const container = styled.div`
+padding: 2px 16px;
+`
+
+export const imagestyle = styled.div`
+height: 300px
+`
+export const Types = styled.div`
+font-size:25px;
+padding: 5px;
+margin-left:100px;
+margin-right:100px;
+background-color: yellow;
+text-align:center;
+font-weight:bold;
+font-family: "Lucida Console", "Courier New", monospace;
+font-style: oblique;
+
+`
+export const contents = styled.div`
+margin-left:200px;
+margin-right:200px;
+`
+export const gridContainer = styled.div`
+display: grid;
+backgroundColor: #2196F3;
+padding: 10px;
+`
+export const gridItem = styled.div`
+backgroundColor: rgba(255, 255, 255, 0.8);
+border: 1px solid rgba(0, 0, 0, 0.8);
+padding: 110px;
+font-size: 30px;
+text-align: center;
+`
+export const center = styled.div`
+
+text-align: center;
+`
+
